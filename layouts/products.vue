df23afbd71c016bf93b54fb0351f43fc9fe901e1
Rab-son <rabsonsayendajnr@gmail.com>
2025-04-13 05:48:43 +0200

    feat: fetching data

---
+++ layouts/products.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <header class="shadow-sm bg-white py-6">
+            <nav class="container mx-auto px-4 flex justify-between">
+                <NuxtLink to="/products">Rab Site Merch</NuxtLink>
+            </nav>
+        </header>
+
+        <div class="container mx-auto p-4">
+            <slot />
+        </div>
+        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+            <ul class="flex gap-4">
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/products">Proudcts</NuxtLink></li>
+            </ul>
+        </footer>
+    </div>
+</template>
+<style scoped>
+.router-link-exact-active {
+    color: #12b488;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, fugiat aliquid. Dolorum dignissimos 
+            dolore recusandae voluptas ad tempore nostrum nulla.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, fugiat aliquid. Dolorum dignissimos 
+            dolore recusandae voluptas ad tempore nostrum nulla.</p>
+    </div>
+</template>
+<script scoped>
+</script>
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
